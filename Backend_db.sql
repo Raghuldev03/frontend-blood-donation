5568e83996a95dcf84d0b675d540301f60ece966
Raghuldev03 <raghulraghul1551@gmail.com>
2025-11-21 21:05:23 +0530

    Initial commit with complete code

--- Backend_db.sql
+++ Backend_db.sql
@@ -8,12 +8,6 @@ select * from admin_details;
 select * from donar_details;
 select * from blood_bank;
 select * from campaigns;
-ALTER TABLE campaigns DROP COLUMN image_url;
-UPDATE campaigns SET blood_type_needed = 'O-' wHERE id = 1;
 select * from blood_quantity;
 select* from blood_request;
-CREATE INDEX idx_registration_number ON blood_bank (registration_number);
-ALTER TABLE blood_quantity ADD CONSTRAINT FKumldjhcq8pm5fln77ysu7muu FOREIGN KEY (registration_number) REFERENCES blood_bank (registration_number);
-ALTER TABLE blood_request DROP COLUMN blood_quantity_id;
-
-
+select * from donation;
